f7f59f7752fb5947895e4ea811b2a79373d8c8ea
Nadin12 <pohlebina.odessa@gmail.com>
2024-10-21 15:22:41 +0300

    Conf file

---
+++ app/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//VIEWS all parameters of views===========================================================
+const VIEWS_TEMPLATE_DEFAULT = 'default';
+const VIEWS_DIR = 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
+const VIEWS_TEMPLATES_DIR = VIEWS_DIR . 'templates' . DIRECTORY_SEPARATOR;
+const VIEWS_COMMON_DIR = VIEWS_DIR . 'common' . DIRECTORY_SEPARATOR;
+const VIEWS_PAGES_DIR = VIEWS_DIR . 'pages' . DIRECTORY_SEPARATOR;
+
+//FUNCTIONS all parameters================================================================
+const FUNCTIONS_DIR = APP_DIR . 'functions' . DIRECTORY_SEPARATOR;
+const STORAGE_NAMES_FILE = APP_DIR . 'storage' . DIRECTORY_SEPARATOR . 'names.json';
+
+//========================================================================================
+const TEXT_SITE_NAME = 'Names';
